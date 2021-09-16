bd7d0ba184f7a255ae6373dd9e87580bca89e177
Nikita Bolshagin <bolshagin.nikita@yandex.ru>
2021-09-16 22:25:31 +0500

    Решение задачи №557

---
+++ leetcode/python/557.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def reverse_words(self, s: str) -> str:
+        result = []
+        for word in s.split():
+            result.append(self.reverse_string([w for w in word]))
+        return ' '.join(result)
+
+    @staticmethod
+    def reverse_string(s: List[str]) -> str:
+        l, m = len(s) - 1, len(s) // 2
+        for idx in range(m):
+            last, t = l - idx, s[idx]
+            s[idx] = s[last]
+            s[last] = t
+        return ''.join(s)
+
+
+if __name__ == '__main__':
+    s = Solution()
+    xs = "Let's take LeetCode contest"
+    ans = s.reverse_words(xs)
+    print(ans)
